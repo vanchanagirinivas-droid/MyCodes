835e05e4a2ae660a744c4ab3e8dfd7330eb93abf
vanchanagirinivas-droid <vanchanagirinivas@gmail.com>
2025-08-16 16:05:31 +0530

    initial commit

---
+++ Helloworld.java
@@ -0,0 +1,7 @@
+ class demo{
+    public static void main(String[] args) {
+    System.out.println("helloworld");
+    System.out.println("hey am nivas");
+    System.out.println("hello world!");
+    }
+ }
