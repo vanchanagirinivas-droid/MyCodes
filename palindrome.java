6fe77d46d4aa49e239289a4bec04aac940c87953
vanchanagirinivas-droid <vanchanagirinivas@gmail.com>
2025-08-30 19:44:09 +0530

    git commit git push origin master

--- palindrome.java
+++ palindrome.java
@@ -1,22 +1,10 @@
 public class palindrome{
     public static void main(String[] args) {
-        String input = "madam"; // Example input
-        if (Palindrome(input)) {
-            System.out.println(input + " is a palindrome.");
-        } else {
-            System.out.println(input + " is not a palindrome.");
-        }
-    }
-    public static boolean Palindrome(String input) {
-        int left = 0;
-        int right = input.length() - 1;
-        while (left < right) {
-            if (input.charAt(left) != input.charAt(right)) {
-                return false;
-            }
-            left++;
-            right--;
-        }
-        return true;
+        String str = "madam";
+        String rev = new StringBuilder(str).reverse().toString();
+        if (str.equals(rev))
+            System.out.println("Palindrome");
+        else
+            System.out.println("Not Palindrome");
     }
 }
